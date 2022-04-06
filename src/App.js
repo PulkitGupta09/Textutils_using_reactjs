b4fcddef01e6e39c0532517a5d19e672b3209f3b
Pulkit Gupta <gpulkit09@gmail.com>
2022-04-06 20:10:07 +0530

    video 13 completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
+import Alert from './components/Alert';
 import TextForm from './components/TextForm';
+import React,{useState} from 'react';
 
 function App() {
+  const [mode,setMode] = useState('light')
+  const [alert, setAlert] = useState(null)
+
+  const showAlert =(message,type)=>{
+      setAlert ({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+        setAlert(null);
+      }, 1500);
+  }
+
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#3B3B3B';
+      showAlert("Dark Mode has been enabled", "success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been enabled", "success");
+    }
+  }
   return (
     <>
-<Navbar title = "TextUtils" about = "About Us"/>
+<Navbar title = "TextUtils" about = "About Us" mode = {mode} toggleMode = {toggleMode}/>
+<Alert alert = {alert}/>
 <div className="container my-3" >
-<TextForm heading = "Enter the text to analyze below" />
+<TextForm heading = "Enter the text to analyze below" mode = {mode} showAlert = {showAlert}/>
+{/* <About /> */}
 </div>
     </>
     );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,14 +6,17 @@ export default function TextForm(props) {
         // console.log("UpperCase was clicked");
         let newText = text.toUpperCase();
         setText(newText); 
+        props.showAlert("Converted to Uppercase!","success");
     }
     const handleLoClick = ()=>{
         let newText = text.toLowerCase();
         setText(newText); 
+        props.showAlert("Converted to Lowercase!","success");
     }
     const handleClearText = ()=>{
         let newText = ""
         setText(newText); 
+        props.showAlert("Converted to Cleartext!","success");
     }
 
     const RemovePunctuation = ()=>{
@@ -21,12 +24,14 @@ export default function TextForm(props) {
             let result = text.replace(puncs, '');
             let newText = result;
             setText(newText);
+            props.showAlert("Converted to RemovedPunctuation!","success");
     }
     const RemoveNumber = ()=>{
             let num = /[0123456789]/g;
             let result = text.replace(num, '');
             let newText = result;
             setText(newText);
+            props.showAlert("Converted to RemovedNumber!","success");
     }
 
     // Copy to Clipboard
@@ -49,10 +54,10 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style = {{color : props.mode==='dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myBox" rows="10"></textarea>
+                <textarea className="form-control" style = {{backgroundColor : props.mode==='dark'?'#3B3B3B':'white',color : props.mode==='dark'?'white':'black'}}  value = {text} onChange = {handleOnChange} id="myBox" rows="10"></textarea>
             </div>
             <button className = "btn btn-primary" onClick = {handleUpClick}>Convert to Uppercase</button>
             <button className = "btn btn-primary mx-2" onClick = {handleLoClick}>Convert to Lowercase</button>
@@ -61,20 +66,23 @@ export default function TextForm(props) {
             <button className = "btn btn-primary mx-2" onClick = {handleClearText}>Clear Text</button>
             <CopyToClipboard text={text}>
                         <span>
-                            <button className="btn btn-outline-success mx-2" onClick={handleCopyClick}>Copy to Clipboard</button>
+                            <button className="btn btn-success mx-2" onClick={handleCopyClick}>Copy to Clipboard</button>
                             <span>{isCopied ? <b> Copied!</b> : false}
                         </span>
                         </span>
             </CopyToClipboard>
         </div>
+        <div className="container" style = {{color : props.mode==='dark'?'white':'black'}}>
         <div className="container my-3">
             <h2>Your text summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
         </div>
         <div className="container my-3">
             <h2>Preview</h2>
-            <p>{text}</p>
+            <p>{text.length>0?text:"Enter Something in the textbox to preview it here"}</p>
         </div>
+        </div>
+
         </>
     )
 }
